634b5c25820f8466a5052b76e5714ee6933b32cf
Lada Anpilogova <lada2fox@gmail.com>
2024-05-19 15:10:58 +0300

    Сохраняю уже существующие файлы

--- src/widget.py
+++ src/widget.py
@@ -1,1 +1,18 @@
-fgfgfgg
+from typing import Union
+import masks
+
+num_user = input("Введите номер карты или счета")
+
+
+def mask_card_3(card_or_account):
+    '''Функция, которая маскирует номер карты или счета '''
+    good_starts = ("Visa", "Cчет", "Maestro", "MasterCard")
+    if card_or_account.startswith(good_starts):
+
+    else:
+        print("Некорректно введен номер карты или счета")
+
+
+
+
+print(mask_card_3(num_user))
