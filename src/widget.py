2c1ec1cb9cabe2627ffcbab5f5e8847a37754bfc
Lada Anpilogova <lada2fox@gmail.com>
2024-05-19 18:51:00 +0300

    Добавлены аннотации к функции data_transformation

--- src/widget.py
+++ src/widget.py
@@ -30,7 +30,7 @@ def mask_card_3(card_or_account: Any) -> str:
         return masks_num_card
 
 
-def data_transformation(data_input):
+def data_transformation(data_input: str) -> str:
     """Функция, которая преобразовывает строку в форматы даты 11.07.2018"""
     day = data_input[8:10]
     month = data_input[5:7]
