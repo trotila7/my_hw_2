acb4935201a66176ce685c1872238874dbdcce0d
Lada Anpilogova <lada2fox@gmail.com>
2024-05-19 14:55:11 +0300

    Модифицирована фукнция mask_card_2

--- src/masks.py
+++ src/masks.py
@@ -30,8 +30,8 @@ def mask_card_2(card: Union[int, str]) -> str:
     num_card_str = list(str(card))
     mask_card_output = list()
     mask_card_output_str_2 = str()
-    if len(num_card_str) == 16:
-        mask_card_output = num_card_str[12:]
+    if len(num_card_str) == 16 or len(num_card_str) == 20:
+        mask_card_output = num_card_str[-6:]
         mask_card_output.insert(0, "**")
         mask_card_output_str_2 = "".join(mask_card_output)
     else:
