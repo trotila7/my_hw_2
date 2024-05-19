94c02277b93e115df00f9045cd40c41232b34a22
Lada Anpilogova <lada2fox@gmail.com>
2024-05-19 17:49:38 +0300

    mypy fixes

--- src/widget.py
+++ src/widget.py
@@ -1,31 +1,27 @@
-from typing import Union
-from masks import mask_card_1
-from masks import mask_card_2
+from typing import Any
+from src.masks import mask_card_1, mask_card_2
 
 num_user = input("Введите номер карты или счета")
 
 
-def mask_card_3(card_or_account: Union[int, str]) -> str:
+def mask_card_3(card_or_account: Any) -> str:
     """Функция, которая маскирует номер карты или счета"""
-    good_starts = ("Visa", "Cчет", "Maestro", "MasterCard")
-    num_card = str()
-    masks_num_card = str()
-    masks_num_account = str()
+    # good_starts = ("Visa", "Cчет", "Maestro", "MasterCard")
     if card_or_account.startswith("Счет"):
         num_card = card_or_account[-20:]
-        masks_num_account = card_or_account[:-20] + mask_card_2(num_card)
+        masks_num_account = str(card_or_account[:-20] + mask_card_2(num_card))
         return masks_num_account
     elif card_or_account.startswith("Visa"):
         num_card = card_or_account[-16:]
-        masks_num_card = card_or_account[:-16] + mask_card_1(num_card)
+        masks_num_card = str(card_or_account[:-16] + mask_card_1(num_card))
         return masks_num_card
     elif card_or_account.startswith("Maestro"):
         num_card = card_or_account[-16:]
-        masks_num_card = card_or_account[:-16] + mask_card_1(num_card)
+        masks_num_card = str(card_or_account[:-16] + mask_card_1(num_card))
         return masks_num_card
     elif card_or_account.startswith("MasterCard"):
         num_card = card_or_account[-16:]
-        masks_num_card = card_or_account[:-16] + mask_card_1(num_card)
+        masks_num_card = str(card_or_account[:-16] + mask_card_1(num_card))
         return masks_num_card
     else:
         masks_num_card = "Введен некорректный номер карты или счета"
