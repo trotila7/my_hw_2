bd47acd498bb7f5afed621a264963aa10a7ac740
Lada Anpilogova <lada2fox@gmail.com>
2024-05-19 15:25:58 +0300

    Реализована функция mask_card_3

--- src/widget.py
+++ src/widget.py
@@ -8,11 +8,13 @@ def mask_card_3(card_or_account):
     '''Функция, которая маскирует номер карты или счета '''
     good_starts = ("Visa", "Cчет", "Maestro", "MasterCard")
     if card_or_account.startswith(good_starts):
-
+        if card_or_account.startswith("Счет"):
+            masks.mask_card_2(card_or_account)
+        else:
+            num_card = card_or_account[-16:]
+            masks.mask_card_1(num_card)
     else:
-        print("Некорректно введен номер карты или счета")
-
-
+        return "Некорректно введен номер карты или счета"
 
 
 print(mask_card_3(num_user))
