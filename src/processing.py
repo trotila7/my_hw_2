5a1994e9f92d72b2c77293258f48a48d5e84bc39
Lada Anpilogova <lada2fox@gmail.com>
2024-05-26 14:56:28 +0300

    fixes processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,5 @@
 from typing import List
 
-list_dict = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-
 
 # Выход функции со статусом по умолчанию EXECUTED
 def filter_dict(input_dict: List[dict], key_filter: str = "EXECUTED") -> List[dict]:
@@ -22,7 +15,3 @@ def filter_dict(input_dict: List[dict], key_filter: str = "EXECUTED") -> List[di
 def sorted_dict(input_dict: List[dict], sorting_order: bool = True) -> List[dict]:
     """Функция, которая сортирует список словарей по дате, по умолчанию: по убыванию"""
     return sorted(input_dict, key=lambda x: x["date"], reverse=sorting_order)
-
-
-print(filter_dict(list_dict))
-print(sorted_dict(list_dict))
