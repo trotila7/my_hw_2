4abfb2793cc21a4ef611c7a28c023a43be69828f
Lada Anpilogova <lada2fox@gmail.com>
2024-07-14 00:21:29 +0300

    тест transaction_amount

---
+++ tests/test_utils.py
@@ -0,0 +1,46 @@
+import json
+import unittest
+from unittest.mock import mock_open,MagicMock, patch
+from src.utils import transaction_amount
+from src.external_api import currency_conversion
+
+
+class TestGetTransactions(unittest.TestCase):
+
+    @patch("builtins.open", new_callable=mock_open, read_data='[{"transaction":"data1"}, {"transaction":"data2"}]')
+    def test_transaction_amount_valid_file(self, mock_file: MagicMock) -> None:
+        expected_data = [{"transaction":"data1"}, {"transaction":"data2"}]
+        result = transaction_amount("fake_path.json")
+        self.assertEqual(result, expected_data)
+        mock_file.assert_called_once_with("fake_path.json", "r", encoding="utf-8")
+
+    @patch("builtins.open", new_callable=mock_open, read_data="[]")
+    def test_transaction_amount_invalid_content(self, mock_file: MagicMock) -> None:
+        result = transaction_amount("fake_path.json")
+        self.assertEqual(result, [])
+        mock_file.assert_called_once_with("fake_path.json", "r", encoding="utf-8")
+
+    @patch("builtins.open", new_callable=mock_open, read_data='')
+    def test_transaction_amount_empty_file(self, mock_file: MagicMock) -> None:
+        result = transaction_amount("fake_path.json")
+        self.assertEqual(result, [])
+        mock_file.assert_called_once_with("fake_path.json", "r", encoding="utf-8")
+
+    @patch("builtins.open", side_effect=FileNotFoundError)
+    def test_transaction_amount_file_not_file(self, mock_file: MagicMock) -> None:
+        result = transaction_amount("fake_path.json")
+        self.assertEqual(result, [])
+        mock_file.assert_called_once_with("fake_path.json", "r", encoding="utf-8")
+
+    @patch("builtins.open", new_callable=mock_open, read_data='{"transaction":"data"}')
+    @patch("json.load", side_effect=json.JSONDecodeError("Expecting value", "", 0))
+    def test_transaction_amount_json_decode_error(self, mock_json_load: MagicMock, mock_file: MagicMock) -> None:
+        result = transaction_amount("fake_path.json")
+        self.assertEqual(result, [])
+        mock_file.assert_called_once_with("fake_path.json", "r", encoding="utf-8")
+        mock_json_load.assert_called_once()
+
+
+
+
+
