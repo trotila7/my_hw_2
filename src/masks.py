a8a8e8ae2e23ed2d813bb140689c78338669d908
Lada Anpilogova <lada2fox@gmail.com>
2024-07-14 21:28:43 +0300

    logging ulits.py and masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,19 @@
+import logging
 from typing import Union
+
+masks_logger = logging.getLogger("masks")
+file_handler = logging.FileHandler("logs/log_masks.log", "w")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+masks_logger.addHandler(file_handler)
+masks_logger.setLevel(logging.INFO)
 
 
 def mask_card_1(card: Union[int, str]) -> str:
+    masks_logger.info("Пользователь ввел номер карты")
     """Функция, которая маскирует номер карты в формат XXXX XX** **** XXXX"""
     num_card_str = list(str(card))
     mask_card_output = list()
-    mask_card_output_str = str()
     if len(num_card_str) == 16:
         for i, num in enumerate(num_card_str):
             if i <= 5:
@@ -18,26 +26,22 @@ def mask_card_1(card: Union[int, str]) -> str:
         mask_card_output.insert(9, " ")
         mask_card_output.insert(14, " ")
         mask_card_output_str = "".join(mask_card_output)
+        masks_logger.info("Карта замаскирована успешно")
     else:
         mask_card_output_str = "Вы ввели некорректный номер карты"
+        masks_logger.debug("Пользователь некорректно ввел номер карты")
     return mask_card_output_str
 
 
 def mask_card_2(card: Union[int, str]) -> str:
     """Функция, которая маскирует номер карты в формат **XXXX"""
     num_card_str = list(str(card))
-    mask_card_output = list()
-    mask_card_output_str_2 = str()
     if len(num_card_str) == 16 or len(num_card_str) == 20:
         mask_card_output = num_card_str[-4:]
         mask_card_output.insert(0, "**")
         mask_card_output_str_2 = "".join(mask_card_output)
+        masks_logger.info("Карта замаскирована успешно")
     else:
         mask_card_output_str_2 = "Вы ввели некорректный номер карты"
+        masks_logger.debug("Пользователь некорректно ввел номер карты")
     return mask_card_output_str_2
-
-
-if __name__ == "__main__":
-    num_card = input("Введите номер карты")
-    print(mask_card_1(num_card))
-    print(mask_card_2(num_card))
--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,29 @@
 import json
+import logging
 from typing import Any
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    filename="logs/log_utils.log",
+    filemode="w",
+)
+
+utils_logger = logging.getLogger("utils")
 
 
 def transaction_amount(file_path: str) -> Any:
     try:
+        utils_logger.info("Открытие json-файла")
         with open(file_path, "r", encoding="utf-8") as file:
             repository = json.load(file)
         if isinstance(repository, (list, dict)):
+            utils_logger.info("Файл открыт успешно")
             return repository
         else:
+            utils_logger.debug("Файл не содержит необходимые данные")
             return []
     except Exception as e:
+        utils_logger.error(f"Ошибка при открытии файла {e}")
         print(f"Ошибка {e}")
         return []
